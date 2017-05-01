bd84deb839b3f0841df205c9cd907659fdf33ee7
toyoil4u <samsungec@gmail.com>
2017-05-01 18:13:25 +0900

    커넥션

---
+++ main/java/java22/DBConnect.java
@@ -0,0 +1,35 @@
+package java22;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+
+    public static Connection connectionMySql(){
+        Connection conn = null;
+
+        String url = "jdbc:mysql://localhost:5600/book_db?useUnicode=true&amp;characterEncoding=utf8";
+        String user = "root";
+        String passwd = "1234";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, passwd);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = DBConnect.connectionMySql();
+        if (conn == null){
+            System.out.println("Failed");
+        } else {
+            System.out.println("Success");
+        }
+    }
+}
